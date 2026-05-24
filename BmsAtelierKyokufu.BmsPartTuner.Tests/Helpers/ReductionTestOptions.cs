dd19e7088fb886d8a745f63876cbf07eae658b01
kassyi <22139176+kassyi@users.noreply.github.com>
2026-05-24 16:09:28 +0900

    test: ReductionTestOptions オプションクラスをテストプロジェクトに導入
    主要機能:
    - テスト実行用オプションクラスの追加
    
    技術的改善:
    - テストパラメータ（BuildBms, CreateFiles, AssertResult, Threshold, StartDef, EndDef 等）をカプセル化する ReductionTestOptions.cs を追加

---
+++ BmsAtelierKyokufu.BmsPartTuner.Tests/Helpers/ReductionTestOptions.cs
@@ -0,0 +1,19 @@
+﻿using BmsAtelierKyokufu.BmsPartTuner.Models;
+using BmsAtelierKyokufu.BmsPartTuner.Services.Bms;
+
+namespace BmsAtelierKyokufu.BmsPartTuner.Tests.Helpers
+{
+    public class ReductionTestOptions
+    {
+        public Action<BmsFileBuilder>? BuildBms { get; set; }
+        public Func<string, List<BmsAudioFile>>? CreateFiles { get; set; }
+        public Action<BmsOptimizationService.ReductionResult>? AssertResult { get; set; }
+        public Action<string>? BeforeExecute { get; set; }
+        public Action<string>? AfterExecute { get; set; }
+        public float? Threshold { get; set; }
+        public int StartDef { get; set; } = 1;
+        public int EndDef { get; set; } = 1;
+        public bool PhysicalDeletion { get; set; }
+        public IEnumerable<string>? Keywords { get; set; }
+    }
+}
